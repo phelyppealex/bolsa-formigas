c2aa06a65177878784ecd914ae3267f401af7eb3
Phelyppe Álex <phelyppesilva@gmail.com>
2024-07-16 03:08:58 -0300

    correção do erro na segmentação da pilha | correção na documentação do código

--- ant-image-processing/src/main/java/br/ufrn/antimageprocessing/controller/ImagemCapsulaController.java
+++ ant-image-processing/src/main/java/br/ufrn/antimageprocessing/controller/ImagemCapsulaController.java
@@ -28,18 +28,19 @@ public class ImagemCapsulaController {
     public Imagem processarImagem(@RequestBody Imagem imagem){
         im = imagem;
 
-        // Total de colunas da pilha e cabeça na imagem
         int totalColunasPilha, totalColunasCabeca, iMenorIntensidade = -1, iMaiorIntensidade = -1, limiar = 0, maiorVale;
-        // A variável métrica diz respeito ao tamanho da pilha em mm
+
+        // A variável métrica diz respeito ao tamanho da pilha em mm (esta métrica é recebida do cliente)
         float
             metrica = (float) imagem.getMetrica(); //milímetros
         
-        // Lendo do banco de imagens
+        // Lendo imagem que veio do cliente
         var imRGB = imagem.getImagem();
 
         //Convertendo a imagem para tons de cinza
         var imGray = Image.rgb2gray(imRGB);
-        var imLogicaPilha = Image.logical(Image.rgb2gray(imRGB));
+
+        // INICIANDO ABAIXO UM ALGORITMO DE LIMIARIZAÇÃO A PARTIR DO HISTOGRAMA
 
         // Criando histograma da imagem
         var imHist = Image.imHist(imGray);
@@ -48,7 +49,7 @@ public class ImagemCapsulaController {
         var novoHist1 = new int[256];
         var novoHist2 = new int[256];
 
-        // Preenchendo as cópias
+        // Inicializando os histogramas
         for(int i = 0; i < 256; i++){
             novoHist1[i] = imHist[i];
             novoHist2[i] = imHist[i];
@@ -64,7 +65,11 @@ public class ImagemCapsulaController {
             if(novoHist2[i+1] > novoHist2[i])
                 novoHist2[i] = novoHist2[i+1];
 
-        // Pegando as modas de cada novo histograma que foi manipulado
+        /*
+         * Pegando as modas de cada novo histograma que foi manipulado
+         * para encontrar os dois maiores picos do histograma e fazer
+         * a limiarização a partir do ponto mais baixo entre eles.
+         */
         int moda1 = imagem.getModaHistograma(novoHist1);
         int moda2 = imagem.getModaHistograma(novoHist2);
 
@@ -84,11 +89,18 @@ public class ImagemCapsulaController {
             }
         }
 
+        /*
+         * Algoritmo para solucionar o problema do caso de a moda 1
+         * seja igual a moda 2 (o programa só conseguiu detectar um pico).
+         * Neste caso é inicializado um novo histograma removendo a segunda
+         * moda para conseguir encontrar a média 1 enfim.
+         */
         if(moda1 == moda2){
             int[] novoArray = new int[iMaiorIntensidade+1];
             for(int i = 0; i < novoArray.length; i++){
                 novoArray[i] = novoHist1[i];
             }
+
             moda1 = imagem.getModaHistograma(novoArray);
 
             // Corrigindo o indice do primeiro pico
@@ -103,7 +115,10 @@ public class ImagemCapsulaController {
         System.out.println("Moda 1: ["+iMenorIntensidade+"] "+moda1);
         System.out.println("Moda 2: ["+iMaiorIntensidade+"] "+moda2);
 
+        // Inicializando o maior vale com a intensidade do primeiro pico
         maiorVale = imHist[iMenorIntensidade];
+
+        // Encontrando o menor vale e definindo o limiar da máscara principal
         for(int i = iMenorIntensidade + 1; i <= iMaiorIntensidade; i++){
             if(maiorVale > imHist[i]){
                 maiorVale = imHist[i];
@@ -120,60 +135,111 @@ public class ImagemCapsulaController {
         //     }
         // }
 
-        // Fazendo limiarização a partir do limiar encontrado
+        // Fazendo segmentação a partir do limiar encontrado
         for(int i = 0; i < imGray.length; i++)
             for(int j = 0; j < imGray[0].length; j++)
                 if(imGray[i][j] <= limiar)
-                    imGray[i][j] = 0;
-                else
                     imGray[i][j] = 255;
+                else
+                    imGray[i][j] = 0;
         
         /*
-        * Aqui acontecem 3 processos.
-        * - Primeiro a imagem "imLimiarizada" é transformada em lógica
-        * - É feito uma dilatação
-        * - É feita uma erosão
-        */
-
+         * Temos abaixo a criação de duas máscaras: a primeira é para
+         * identificar melhor a formiga, porque em alguns momentos é
+         * complicado identificar um limiar que consiga pegar a pilha 
+         * também; a segunda é apenas para conseguir resgatar a pilha
+         * que as vezes é perdida na primeira máscara. Em seguida, as duas
+         * são mescladas, pois o código seguinte depende dos dois objetos
+         * identificados corretamente em apenas uma máscara para
+         * funcionar corretamente.
+         */
         var imLogica = Image.logical(imGray);
+        var imLogicaPilha = Image.logical(Image.rgb2gray(imRGB));
 
         for(int i = 0; i < imGray.length; i++){
             for(int j = 0; j < imGray[0].length; j++){
-                imLogica[i][j] = !imLogica[i][j];
                 imLogicaPilha[i][j] = !imLogicaPilha[i][j];
             }
         }
 
+        /*
+         * Erodindo e dilatando imagem para remoção de ruído
+         * na máscara de identificação da pilha.
+         */
         imLogicaPilha = Image.bwOpen(
             imLogicaPilha,
             20
         );
 
-        im.setMascara(Image.bw2rgb(imLogicaPilha));
+        /*
+         * Fazendo identificação da largura da pilha na imagem
+         * logo, porque é preciso mesclá-la de imediato a máscara
+         * principal para não obter erros na contagem de regiões.
+         */
+        var fgPixelsPilha = new boolean[imLogica[0].length];
+        for(int i = 0; i < imLogica.length; i++){
+            for(int j = 0; j < imLogica[0].length; j++){
+                if(imLogicaPilha[i][j] && !fgPixelsPilha[j])
+                    fgPixelsPilha[j] = true;
+            }
+        }
 
+        // Guardando índices de início e de fim das colunas correspondentes a pilha
+        int inicioPilha = -1, fimPilha = -1;
+        for(int i = fgPixelsPilha.length-1; i > 0; i--){
+            if(fgPixelsPilha[i]){
+                if(inicioPilha == -1)
+                    inicioPilha = i;
+                if(fgPixelsPilha[i-1] == false){
+                    fimPilha = i;
+                    break;
+                }
+            }
+        }
+        
+        totalColunasPilha = inicioPilha-fimPilha+1;
+
+        // Mesclando as duas máscaras
+        for(int i = 0; i < imLogica.length; i++){
+            for(int j = inicioPilha; j <= fimPilha; j++){
+                imLogica[i][j] = imLogicaPilha[i][j];
+            }
+        }
+
+        // Dilatação e erosão para atenuação dos brilhos existentes na imagem
         imLogica = Image.bwClose(
             imLogica,
             15
         );
 
+        // Erosão e dilatação para remoção do máximo de ruídos possível
         imLogica = Image.bwOpen(
             imLogica,
             40
         );
 
-        //                                                                                       INICIO CONTAGEM DE REGIOES
+        // INICIO CONTAGEM DE REGIOES
+        // Matriz de rotulos para rotular cada objeto da região de interesse
         int[][] matrizRotulos = new int[imLogica.length][imLogica[0].length];
         
+        // Inicialização da matriz
         for(int i = 0; i < imLogica.length; i++){
             for(int j = 0; j < imLogica[0].length; j++){
                 matrizRotulos[i][j] = 0;
             }
         }
 
+        // Variável que conta a quantidade de objetos e é utilizada para rotular os mesmos
         int rotulo = 0;
 
         for(int i = 0; i < imLogica.length; i++){
             for(int j = 0; j < imLogica[0].length; j++){
+                /*
+                 * Ao encontrar uma região de interesse rotula o objeto
+                 * utilizando a função "mapearRegiao" que faz uma busca
+                 * em largura na máscara e rotula todo o objeto na
+                 * matriz de rótulos
+                 */
                 if(imLogica[i][j] && matrizRotulos[i][j] == 0){
                     rotulo += 1;
                     matrizRotulos = imagem.mapearRegiao(imLogica, i, j, rotulo, matrizRotulos);
@@ -182,6 +248,14 @@ public class ImagemCapsulaController {
         }
         System.out.println("Quantidade de regioes: "+ rotulo);
 
+        // FIM CONTAGEM DE REGIOES
+
+        /*
+         * Algoritmo para remover quaisquer resquícios
+         * de objetos de foreground indesejados ou errôneos,
+         * mantendo apenas os dois maiores objetos da máscara:
+         * a formiga e a pilha.
+         */
         if(rotulo > 2){
             int[] qntPixelPorRegiao = new int[rotulo];
 
@@ -223,13 +297,11 @@ public class ImagemCapsulaController {
             }
         }
 
-        //                                                                                       FIM CONTAGEM DE REGIOES
-
-        // Gerando imagem final com tons de cinza
-        for(int i = 0; i < imGray.length; i++)
-            for(int j = 0; j < imGray[0].length; j++)
-                if(!imLogica[i][j])
-                    imGray[i][j] = 0;
+        // // Gerando imagem final com tons de cinza
+        // for(int i = 0; i < imGray.length; i++)
+        //     for(int j = 0; j < imGray[0].length; j++)
+        //         if(!imLogica[i][j])
+        //             imGray[i][j] = 0;
         
         // Gerando imagem final colorida
         for(int i = 0; i < imGray.length; i++)
@@ -244,30 +316,12 @@ public class ImagemCapsulaController {
         * pixels de foreground.
         */
         var fgPixels = new boolean[imLogica[0].length];
-        var fgPixelsPilha = new boolean[imLogica[0].length];
         for(int i = 0; i < imLogica.length; i++){
             for(int j = 0; j < imLogica[0].length; j++){
                 if(imLogica[i][j] && !fgPixels[j])
                     fgPixels[j] = true;
-                if(imLogicaPilha[i][j] && !fgPixelsPilha[j])
-                    fgPixelsPilha[j] = true;
             }
         }
-        
-        // Guardando índices de início e de fim das colunas correspondentes a pilha
-        int inicioPilha = -1, fimPilha = -1;
-        for(int i = fgPixelsPilha.length-1; i > 0; i--){
-            if(fgPixelsPilha[i]){
-                if(inicioPilha == -1)
-                    inicioPilha = i;
-                if(fgPixelsPilha[i-1] == false){
-                    fimPilha = i;
-                    break;
-                }
-            }
-        }
-        
-        totalColunasPilha = inicioPilha-fimPilha+1;
         
         // Guardando índices de início e de fim das colunas correspondentes a cabeça
         int inicioCabeca = -1, fimCabeca = -1;
@@ -381,7 +435,7 @@ public class ImagemCapsulaController {
         String alturaVerticeMandibulaStr = df.format(alturaVerticeMandibulaMm);
         String larguraCabecaStr = df.format(larguraCabecaMm);
 
-        //im.setMascara(Image.bw2rgb(imLogica));
+        im.setMascara(Image.bw2rgb(imLogica));
 
         im.setIndicesAltura(indicesAltura);
         im.setIndicesLargura(indicesLargura);
